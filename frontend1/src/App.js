9421d99a713fda976cc595d8f4ad750e5d8ade30
Ganeshkumar208 <ganeshkumarakula17@gmail.com>
2024-12-31 17:59:19 +0530

    added new offer letter screen and changes in app only display navbar when in either of screen except home

--- frontend1/src/App.js
+++ frontend1/src/App.js
@@ -1,20 +1,29 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, useLocation } from 'react-router-dom';
 import Home from './components/Home';
 import Product from './components/Product';
 import NavBar from './components/NavBar';
+import OfferLetter from './components/OfferLetter';
 
 const App = () => {
+  const location = useLocation();
+
   return (
-    <Router>
-      <NavBar />
+    <>
+      {location.pathname !== '/' && <NavBar />}
       <Routes>
         <Route exact path="/" element={<Home />} />
         <Route path="/product" element={<Product />} />
+        <Route path="/offer-letter" element={<OfferLetter />} />
       </Routes>
-    </Router>
+    </>
   );
 };
 
-export default App;
+const AppWrapper = () => (
+  <Router>
+    <App />
+  </Router>
+);
 
+export default AppWrapper;
--- frontend1/src/components/NavBar.js
+++ frontend1/src/components/NavBar.js
@@ -1,12 +1,79 @@
+// import React from 'react';
+// import { Link, useNavigate } from 'react-router-dom';
+// import './NavBarStyles.css';
+// import { FiLogOut } from 'react-icons/fi';
+// import { useLocation } from "react-router-dom";
+// // import app from '../Images/icon.png'
+
+// const NavBar = () => {
+//     // const user = location.state?.user;
+//     const navigate = useNavigate();
+//     const location = useLocation();
+//     const user = JSON.parse(localStorage.getItem("user"));
+
+//     const handleLogout = () => {
+//         localStorage.removeItem("user");
+//         navigate('/');
+//     };
+
+//     return (
+//         <nav className="navbar">
+//             <ul className="navbar-list">
+//                 {/* <li>
+//                     <img src={app} style={{ width: '20px', height: '20px' }}></img>
+//                 </li> */}
+//                 <li className="navbar-item">
+//                     <Link to="/" className="navbar-link">Home</Link>
+//                 </li>
+//                 <li className="navbar-item">
+//                     <Link to="/product" className="navbar-link">Product</Link>
+//                 </li>
+//                 <li className="navbar-item">
+//                     <Link to="/offer-letter" className="navbar-link">Offer</Link>
+//                 </li>
+//                 {user && location.pathname === '/product' ? (
+//                     <li>vcvg
+//                         <div className="username" onClick={handleLogout}>
+//                             Hello! <span className="username-display">{user.displayName?.split(" ")[0] || 'Guest'}</span>
+//                         </div>
+//                     </li>
+//                 ) : null}
+
+//                 {user && location.pathname === '/product' ? (
+//                     <li>
+//                         <div className="logout-button" onClick={handleLogout}>
+//                             <FiLogOut size={20} style={{ cursor: 'pointer', color: '#fed0bb', fontWeight: 'bolder', marginLeft: '10px' }} />
+//                         </div>
+//                     </li>
+//                 ) : null}
+//             </ul>
+//         </nav>
+//     );
+// };
+
+// export default NavBar;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 import React from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import './NavBarStyles.css';
 import { FiLogOut } from 'react-icons/fi';
 import { useLocation } from "react-router-dom";
-// import app from '../Images/icon.png'
 
 const NavBar = () => {
-    // const user = location.state?.user;
     const navigate = useNavigate();
     const location = useLocation();
     const user = JSON.parse(localStorage.getItem("user"));
@@ -19,31 +86,26 @@ const NavBar = () => {
     return (
         <nav className="navbar">
             <ul className="navbar-list">
-                {/* <li>
-                    <img src={app} style={{ width: '20px', height: '20px' }}></img>
-                </li> */}
                 <li className="navbar-item">
                     <Link to="/" className="navbar-link">Home</Link>
                 </li>
                 <li className="navbar-item">
                     <Link to="/product" className="navbar-link">Product</Link>
                 </li>
-                {user && location.pathname === '/product' ? (
-                    <li>
-                        <div className="username" onClick={handleLogout}>
-                            Hello! <span className="username-display">{user.displayName?.split(" ")[0] || 'Guest'}</span>
-                        </div>
-                    </li>
-                ) : null}
-
-                {user && location.pathname === '/product' ? (
-                    <li>
-                        <div className="logout-button" onClick={handleLogout}>
-                            <FiLogOut size={20} style={{ cursor: 'pointer', color: '#fed0bb', fontWeight: 'bolder', marginLeft: '10px' }} />
-                        </div>
-                    </li>
-                ) : null}
+                <li className="navbar-item">
+                    <Link to="/offer-letter" className="navbar-link">Offer Letter</Link>
+                </li>
             </ul>
+            {user && location.pathname === '/product' && (
+                <div className="user-actions">
+                    <div className="username" onClick={handleLogout}>
+                        Hello! <span className="username-display">{user.displayName?.split(" ")[0] || 'Guest'}</span>
+                    </div>
+                    <div className="logout-button" onClick={handleLogout}>
+                        <FiLogOut size={20} style={{ color: '#fed0bb', fontWeight: 'bolder', marginLeft: '10px' }} />
+                    </div>
+                </div>
+            )}
         </nav>
     );
 };
